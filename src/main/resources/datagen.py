501b0425f99ad6929cb811f4c33cc83ec66837f7
AtobaAzul <atoba.azul.radical@gmail.com>
2026-09-04 15:14:58 -0300

    refactored datagen, fixed oxidized/waxed recipes, added waxed lang

--- src/main/resources/datagen.py
+++ src/main/resources/datagen.py
@@ -36,54 +36,44 @@ recipe_dict = {
 
 item_names = []
 
-# Tags
-with open(
-    "data/caverns_and_chasms/tags/items/experience_boost_items.json", "w"
-) as file:
-    lang = {"values": []}
-    for type in types:
-        lang["values"].append("moonsweaponry:golden_" + type)
-    json.dump(lang, file, indent=4)
-    file.close()
 
-with open("data/caverns_and_chasms/tags/items/magic_damage_items.json", "w") as file:
-    lang = {"values": []}
-    for type in types:
-        lang["values"].append("ccww:silver_" + type)
-    json.dump(lang, file, indent=4)
-    file.close()
+def create_item_tag(tag: str, items: list):
+    """Creates an item tag for the given list of items
 
-with open(
-    "data/caverns_and_chasms/tags/items/slowness_inflicting_items.json", "w"
-) as file:
-    lang = {"values": []}
+    Args:
+        tag (str): The tag to create
+        items (list): The items to add to the tag
+    """
 
-    for type in types:
-        lang["values"].append("ccww:necromium_" + type)
-    json.dump(lang, file, indent=4)
-    file.close()
+    mod = tag.split(":")[0]
+    _tag = tag.split(":")[1]
+    with open("data/" + mod + "/tags/items/" + _tag + ".json", "w") as file:
+        data = {"values": items}
+        json.dump(data, file, indent=4)
+        file.close()
 
+
+########## Tags
+exp_boost_items = []
+magic_dmg_items = []
+slowness_items = []
 
 for type in types:
-    # Model data
-    for material in materials:
-        item_name = f"{material}_{type}".format(type=type, material=material)
-        item_names.append(item_name)
+    exp_boost_items.append("moonsweaponry:golden_" + type)
+    magic_dmg_items.append("ccww:silver_" + type)
+    slowness_items.append("ccww:necromium_" + type)
 
-        # Copper waxed variants
-        if "copper" in material:
-            print("Creating waxed variant for: " + item_name)
-            with open(
-                "assets/ccww/models/item/waxed_" + item_name + ".json", "w"
-            ) as file:
-                lang = {
-                    "parent": "ccww:item/" + item_name,
-                }
-                json.dump(lang, file, indent=4)
-                file.close()
+create_item_tag(
+    "caverns_and_chasms:experience_boost_items", ["moonsweaponry:golden_" + type]
+)
+create_item_tag("caverns_and_chasms:magic_damage_items", ["ccww:silver_" + type])
+create_item_tag(
+    "caverns_and_chasms:slowness_inflicting_items", ["ccww:necromium_" + type]
+)
 
-        model_file = open("assets/ccww/models/item/" + item_name + ".json", "w")
-        model_data = """{{
+########## Models
+
+model_data_str = """{{
   "parent": "minecraft:item/handheld",
   "loader": "forge:separate_transforms",
   "textures": {{
@@ -117,7 +107,15 @@ for type in types:
   }}
 }}"""
 
-        model_data = model_data.format(
+
+def create_weapon_model_data(type: str, material: str):
+    item_name = f"{material}_{type}".format(type=type, material=material)
+    item_names.append(item_name)
+
+    # Copper waxed variants
+    if "copper" in material:
+        model_file = open("assets/ccww/models/item/waxed_" + item_name + ".json", "w")
+        model_data = model_data_str.format(
             item_name=item_name,
             base=("netherite" if material == "necromium" else "iron"),
             type=type,
@@ -130,64 +128,122 @@ for type in types:
             )
 
         model_file.writelines(model_data)
-        print("Created model file for: " + item_name)
+        print("Created waxed model file for: " + item_name)
         model_file.close()
+    
+    # Normal models
+    model_file = open("assets/ccww/models/item/" + item_name + ".json", "w")
 
-        # Better combat data
-        weapon_attributes_file = open(
-            "data/ccww/weapon_attributes/" + item_name + ".json", "w"
+    model_data = model_data_str.format(
+        item_name=item_name,
+        base=("netherite" if material == "necromium" else "iron"),
+        type=type,
+    )
+
+    if "copper" in material:
+        model_data = model_data.replace(
+            "moonsweaponry:item/iron_hammer_handheld",
+            "ccww:item/copper_hammer_handheld",
         )
-        weapon_attributes_data = """{{
-    "parent": "moonsweaponry:base/{type}"
-}}"""
-        weapon_attributes_data = weapon_attributes_data.format(type=type)
-        weapon_attributes_file.writelines(weapon_attributes_data)
-        print("Created weapon attributes file for: " + item_name)
-        weapon_attributes_file.close()
 
-        # recipes
-        with open("data/ccww/recipes/" + item_name + ".json", "w") as file:
-            crafting_material = ...
-            if material != "necromium":
-                if material == "copper":
-                    crafting_material = "forge:storage_blocks/" + material
-                elif "_" in material:
-                    crafting_material = "forge:storage_blocks/" + material.replace(
-                        "block", ""
-                    )
-                else:
-                    crafting_material = "forge:ingots/" + material
+    model_file.writelines(model_data)
+    print("Created model file for: " + item_name)
+    model_file.close()
 
-                recipe = {
-                    "type": "minecraft:crafting_shaped",
-                    "pattern": recipe_dict[type],
-                    "key": {
-                        "A": {"tag": crafting_material},
-                        "B": {"tag": "forge:rods/wooden"},
-                    },
-                    "result": {"item": "ccww:" + item_name},
-                }
-                print("Created recipe for: " + item_name)
-                json.dump(recipe, file, indent=4)
-                file.close()
-            else:
-                recipe = {
-                    "type": "minecraft:smithing_transform",
-                    "addition": {"tag": "forge:ingots/necromium"},
-                    "base": {"item": "moonsweaponry:diamond_"+type},
-                    "result": {"item": "ccww:" + item_name},
-                    "template": {
-                        "item": "minecraft:netherite_upgrade_smithing_template"
-                    },
-                }
-                print("Created recipe for: " + item_name)
-                json.dump(recipe, file, indent=4)
-                file.close()
+
+########## Weapon attributes
+def create_weapon_attributes(type: str, material: str):
+    item_name = f"{material}_{type}".format(type=type, material=material)
+    file = open("data/ccww/weapon_attributes/" + item_name + ".json", "w")
+    data = {
+        "parent": "moonsweaponry:base/" + type,
+    }
+    json.dump(data, file, indent=4)
+    print("Created weapon attributes file for: " + item_name)
+    file.close()
+
+
+########## Recipes
+def create_weapon_shaped_recipe(type: str, material: str, ingredient: str, tag: bool):
+
+    item_name = f"{material}_{type}".format(type=type, material=material)
+
+    file = open("data/ccww/recipes/crafting/" + item_name + ".json", "w")
+    data = {
+        "type": "minecraft:crafting_shaped",
+        "pattern": recipe_dict[type],
+        "key": {
+            "B": {"tag": "forge:rods/wooden"},
+        },
+        "result": {"item": "ccww:" + item_name},
+    }
+    data["key"]["A"] = {"tag": ingredient} if tag else {"item": ingredient}
+    json.dump(data, file, indent=4)
+    print("Created recipe file for: " + item_name)
+    file.close()
+
+
+def create_weapon_smithing_recipe(type: str, material: str, ingredient: str, base: str):
+    item_name = f"{material}_{type}".format(type=type, material=material)
+
+    file = open("data/ccww/recipes/smithing/" + item_name + ".json", "w")
+    data = {
+        "type": "minecraft:smithing",
+        "base": {"item": base},
+        "addition": {"item": ingredient},
+        "result": {"item": "ccww:" + item_name},
+    }
+    json.dump(data, file, indent=4)
+    print("Created smithing recipe file for: " + item_name)
+    file.close()
+
+
+for type in types:
+    # Model data
+    for material in materials:
+        item_name = f"{material}_{type}".format(type=type, material=material)
+        item_names.append(item_name)  # For lang file creation.
+
+        create_weapon_model_data(type, material)
+        create_weapon_attributes(type, material)
+        # Recipes
+        if "copper" in material:
+            create_weapon_shaped_recipe(
+                type,
+                material,
+                (
+                    "minecraft:copper_block"
+                    if material == "copper"
+                    else "minecraft:" + material
+                ),
+                False,
+            )
+            create_weapon_shaped_recipe(
+                type,
+                "waxed_" + material,
+                (
+                    "minecraft:waxed_copper_block"
+                    if material == "copper"
+                    else "minecraft:waxed_" + material
+                ),
+                False,
+            )
+        elif material == "necromium":
+            create_weapon_smithing_recipe(
+                type,
+                material,
+                "caverns_and_chasms:necromium_ingot",
+                "moonsweaponry:diamond_" + type,
+            )
+        else:
+            create_weapon_shaped_recipe(type, material, "forge:ingots/silver", True)
 
 ## Lang files.
 lang = {}
 _file = open("assets/ccww/lang/en_us.json", "w")
 for name in item_names:
     lang["item.ccww." + name] = name.replace("_", " ").title()
+    if "copper" in name:
+        lang["item.ccww.waxed_" + name] = "Waxed " + name.replace("_", " ").title()
 json.dump(lang, _file, indent=4)
 _file.close()
